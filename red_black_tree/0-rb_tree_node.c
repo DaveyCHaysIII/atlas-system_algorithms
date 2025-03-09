55311f73a40bb7356047b76f17d83d5844601c9b
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-03-09 11:02:22 -0500

    added add node

--- red_black_tree/0-rb_tree_node.c
+++ red_black_tree/0-rb_tree_node.c
@@ -12,5 +12,14 @@
 
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
+	rb_tree_t *new_node = malloc(sizeof(rb_tree_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->color = color;
+
+	return (new_node);
 }
