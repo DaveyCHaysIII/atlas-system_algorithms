ec6cfeb5d254516047ac0e179f6f9c1a029c3dd8
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-03-16 13:24:55 -0500

    got consistent results from main for insert

--- red_black_tree/2-rb_tree_insert.c
+++ red_black_tree/2-rb_tree_insert.c
@@ -1,8 +1,11 @@
 #include "rb_trees.h"
 
-
 rb_tree_t *bst_insert(rb_tree_t **tree, int value);
 void tree_fix(rb_tree_t **tree, rb_tree_t *node);
+rb_tree_t *find_uncle(rb_tree_t *node);
+void rotate_fix(rb_tree_t **tree, rb_tree_t *node);
+void right_rotate(rb_tree_t **tree, rb_tree_t *node);
+void left_rotate(rb_tree_t **tree, rb_tree_t *node);
 
 /**
  * rb_tree_insert - inserts a node into a tree
@@ -18,8 +21,9 @@ rb_tree_t *rb_tree_insert(rb_tree_t **tree, int value)
 
 	if (node == NULL)
 		return (NULL);
-
+	(*tree)->color = BLACK;
 	tree_fix(tree, node);
+	(*tree)->color = BLACK;
 	return (node);
 }
 
@@ -43,7 +47,7 @@ rb_tree_t *bst_insert(rb_tree_t **tree, int value)
 	{
 		if ((*tree)->left == NULL)
 		{
-			new_node = binary_tree_node(*tree, value);
+			new_node = rb_tree_node(*tree, value, RED);
 			return ((*tree)->left = new_node);
 		}
 		return (bst_insert(&(*tree)->left, value));
@@ -52,7 +56,7 @@ rb_tree_t *bst_insert(rb_tree_t **tree, int value)
 	{
 		if ((*tree)->right == NULL)
 		{
-			new_node = binary_tree_node(*tree, value);
+			new_node = rb_tree_node(*tree, value, RED);
 			return ((*tree)->right = new_node);
 		}
 		return (bst_insert(&(*tree)->right, value));
@@ -74,14 +78,143 @@ void tree_fix(rb_tree_t **tree, rb_tree_t *node)
 
 	if (!node)
 		return;
-	if (node->parent == NULL)
+	if (node == *tree)
 	{
 		node->color = BLACK;
 		return;
 	}
-	if (node->parent == BLACK)
+	if (node->parent->color == BLACK)
 		return;
 
 	uncle = find_uncle(node);
-	if (!uncle)
+	if (uncle && uncle->color == RED)
+	{
+		node->parent->color = BLACK;
+		node->parent->parent->color = RED;
+		uncle->color = BLACK;
+		tree_fix(tree, node->parent->parent);
+	}
+	else
+		rotate_fix(tree, node);
+}
+
+/**
+ * find_uncle - finds the uncle and returns it
+ * @node: the node to investigate
+ *
+ * Return: pointer or NULL
+ */
+
+rb_tree_t *find_uncle(rb_tree_t *node)
+{
+	rb_tree_t *parent;
+
+	if (!node || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	parent = node->parent;
+	if (parent == parent->parent->left)
+		return (parent->parent->right);
+	return (parent->parent->left);
+}
+
+/**
+ * rotate_fix - fixes the tree with rotatons
+ * @tree: the tree in question
+ * @node: the node in question
+ *
+ * Return: no return
+ */
+
+void rotate_fix(rb_tree_t **tree, rb_tree_t *node)
+{
+	rb_tree_t *parent, *grandparent;
+
+	parent = node->parent;
+	if (parent == *tree)
+		return;
+	grandparent = node->parent->parent;
+
+	if (parent == grandparent->left)
+	{
+		if (node == parent->right)
+		{
+			left_rotate(tree, parent);
+			node = parent;
+			parent = node->parent;
+		}
+		right_rotate(tree, grandparent);
+		parent->color = BLACK;
+		grandparent->color = RED;
+	}
+	else
+	{
+		if (node == parent->left)
+		{
+			right_rotate(tree, parent);
+			node = parent;
+			parent = node->parent;
+		}
+		left_rotate(tree, grandparent);
+		parent->color = BLACK;
+		grandparent->color = RED;
+	}
+	(*tree)->color = BLACK;
+}
+
+/**
+ * right_rotate - rotates a tree to the right
+ * @tree: the tree in question
+ * @node: the specific node to pivot
+ *
+ * Return: no return
+ */
+
+void right_rotate(rb_tree_t **tree, rb_tree_t *node)
+{
+	rb_tree_t *left_child;
+
+	left_child = node->left;
+	node->left = left_child->right;
+
+	if (left_child->right)
+		left_child->right->parent = node;
+	left_child->parent = node->parent;
+	if (!node->parent)
+		*tree = left_child;
+	else if (node == node->parent->right)
+		node->parent->right = left_child;
+	else
+		node->parent->left = left_child;
+	left_child->right = node;
+	node->parent = left_child;
+}
+
+
+/**
+ * left_rotate - rotates a tree to the left
+ * @tree: the tree in question
+ * @node: the specific node to pivot
+ *
+ * Return: no return
+ */
+
+void left_rotate(rb_tree_t **tree, rb_tree_t *node)
+{
+	rb_tree_t *right_child;
+
+	right_child = node->right;
+	node->right = right_child->left;
+
+	if (right_child->left)
+		right_child->left->parent = node;
+	right_child->parent = node->parent;
+	if (!node->parent)
+		*tree = right_child;
+	else if (node == node->parent->left)
+		node->parent->left = right_child;
+	else
+		node->parent->right = right_child;
+	right_child->left = node;
+	node->parent = right_child;
 }
