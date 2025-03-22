4b6849159f7feeb913b2e6181531897085eec213
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-03-22 10:58:41 -0500

    added add vertex

---
+++ graphs/1-graph_add_vertex.c
@@ -0,0 +1,43 @@
+#include "graphs.h"
+
+/**
+ * graph_add_vertex - adds a vertex to the graph->vertices linked list
+ * @graph: the graph in question
+ * @str: the content to save in the vertex
+ *
+ * Return: pointer to new vertex
+ */
+
+vertex_t *graph_add_vertex(graph_t *graph, const char *str)
+{
+	vertex_t *new_vertex, *current;
+
+	new_vertex = malloc(sizeof(vertex_t));
+	if (!new_vertex)
+		return (NULL);
+
+	new_vertex->content = (char *)str;
+	new_vertex->nb_edges = 0;
+	new_vertex->edges = NULL;
+	new_vertex->next = NULL;
+
+	graph->nb_vertices++;
+	if (graph->vertices == NULL)
+	{
+		new_vertex->index = 0;
+		graph->vertices = new_vertex;
+		return (new_vertex);
+	}
+	for (current = graph->vertices;
+	     current->next != NULL;
+	     current = current->next)
+	{
+		if (strcmp(current->content, str) == 0)
+			return (NULL);
+	}
+
+	new_vertex->index = current->index + 1;
+	current->next = new_vertex;
+
+	return (new_vertex);
+}
