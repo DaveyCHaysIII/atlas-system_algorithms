af2a2807e2d286020a3f626b41860b157ef467b6
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-03-30 10:15:08 -0500

    initialized left/right

--- huffman_coding/heap/binary_tree_node.c
+++ huffman_coding/heap/binary_tree_node.c
@@ -19,6 +19,8 @@ binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
 	if (!new_node)
 		return (NULL);
 	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 	new_node->data = data;
 
 	return (new_node);
