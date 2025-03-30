29752b6b6134abf4273d00bae97b22413bcbc861
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-03-30 10:02:11 -0500

    initial commit

---
+++ huffman_coding/heap/heap_create.c
@@ -0,0 +1,20 @@
+#include "heap.h"
+
+/**
+ * heap_create - create and initialize a heap
+ * @data_cmp: function pointer to add to member data_cmp
+ *
+ * Return: pointer to created heap
+ */
+
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	heap_t *new_heap;
+
+	new_heap = malloc(sizeof(heap_t));
+	new_heap->size = 0;
+	new_heap->data_cmp = data_cmp;
+	new_heap->root = NULL;
+
+	return (new_heap);
+}
