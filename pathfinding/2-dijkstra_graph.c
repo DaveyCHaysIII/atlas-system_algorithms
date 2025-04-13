a495efe7031f809f6797530326d9d3400176c145
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-04-13 15:29:54 -0500

    taking a break

--- pathfinding/2-dijkstra_graph.c
+++ pathfinding/2-dijkstra_graph.c
@@ -1,4 +1,5 @@
 #include "pathfinding.h"
+#include <limits.h>
 
 /**
  * dijkstra_graph - the dijkstra traversal of a graph
@@ -13,5 +14,25 @@ queue_t *dijkstra_graph(graph_t *graph,
 			vertex_t const *start,
 			vertex_t const *target)
 {
+
+	if (!graph || !start || !target)
+		return (NULL);
+
+	int *visited = calloc(graph->nb_vertices, sizeof(int));
+	int *dist = malloc(graph->nb_vertices * sizeof(int));
+	int **prev = malloc(sizeof(vertex_t *) * graph->nb_vertices);
+
+	if (!visited || !dist || !prev)
+		return (NULL);
+
+	for (size_t i = 0; i < graph->nb_vertices; i++)
+	{
+		dist[i] = INT_MAX;
+		prev[i] = NULL;
+	}
+	dist[start->index] = 0;
+
+
+
 
 }
